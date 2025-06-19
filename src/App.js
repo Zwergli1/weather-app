a3c002772c337373bebe3e3e60f12d21afcd8a2f
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2025-06-19 22:04:55 +0200

    Included Search engine and changed title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Weather App</h1>
         <SearchEngine />
       </header>
     </div>
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,5 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function SearchEngine() {
-  return <h2>Hello from SearchEngine!</h2>;
+  let [city, setCity] = useState(" ");
+  let [submittedCity, setSubmittedCity] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSubmittedCity(city);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let [temperature, setTemperature] = useState(null);
+
+  function showTemperature(response) {
+    setTemperature(response.data.main.temp);
+  }
+
+  let [humidity, setHumidity] = useState(null);
+
+  function showHumidity(response) {
+    setHumidity(response.data.main.humidity);
+  }
+
+  let [description, setDescription] = useState(null);
+
+  function showDescription(response) {
+    setDescription(response.data.weather[0].description);
+  }
+
+  let [wind, setWind] = useState(null);
+
+  function showWind(response) {
+    setWind(response.data.wind.speed);
+  }
+
+  let [iconID, setIconID] = useState(null);
+
+  function showIconID(response) {
+    setIconID(response.data.weather[0].icon);
+  }
+
+  let [icon, setIcon] = useState(null);
+
+  function showIcon(response) {
+    setIcon(`http://openweathermap.org/img/wn/${iconID}@2x.png`);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSubmittedCity(city);
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=597c40c39084687093b091cd48b366f8`
+      )
+      .then(showTemperature)
+      .then(showHumidity)
+      .then(showDescription)
+      .then(showWind)
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+      {submittedCity && (
+        <ul>
+          <li>
+            The temperature in {submittedCity} is {Math.round(temperature)}°C
+          </li>
+          <li>Description: {description}</li>
+          <li>Humidity: {humidity}%</li>
+          <li>Wind: {Math.round(wind)} km/h</li>
+          <li>
+            <img src={icon} />{" "}
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 }
