cffea40d70ab282d89a7726a38924c7152aba5ad
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2025-06-19 22:14:59 +0200

    Changed SearchEngine code

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -2,90 +2,55 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function SearchEngine() {
-  let [city, setCity] = useState(" ");
-  let [submittedCity, setSubmittedCity] = useState("");
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    setSubmittedCity(city);
+    let apiKey = "34ae1065362d42545661451bda2b8a1f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
 
-  let [temperature, setTemperature] = useState(null);
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
 
-  function showTemperature(response) {
-    setTemperature(response.data.main.temp);
-  }
-
-  let [humidity, setHumidity] = useState(null);
-
-  function showHumidity(response) {
-    setHumidity(response.data.main.humidity);
-  }
-
-  let [description, setDescription] = useState(null);
-
-  function showDescription(response) {
-    setDescription(response.data.weather[0].description);
-  }
-
-  let [wind, setWind] = useState(null);
-
-  function showWind(response) {
-    setWind(response.data.wind.speed);
-  }
-
-  let [iconID, setIconID] = useState(null);
-
-  function showIconID(response) {
-    setIconID(response.data.weather[0].icon);
-  }
-
-  let [icon, setIcon] = useState(null);
-
-  function showIcon(response) {
-    setIcon(`http://openweathermap.org/img/wn/${iconID}@2x.png`);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    setSubmittedCity(city);
-
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=597c40c39084687093b091cd48b366f8`
-      )
-      .then(showTemperature)
-      .then(showHumidity)
-      .then(showDescription)
-      .then(showWind)
-      .catch((error) => {
-        console.error("Error fetching weather data:", error);
-      });
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="search" onChange={updateCity} />
-        <input type="submit" value="Search" />
-      </form>
-      {submittedCity && (
+  if (loaded) {
+    return (
+      <div>
+        {form}
         <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
           <li>
-            The temperature in {submittedCity} is {Math.round(temperature)}°C
-          </li>
-          <li>Description: {description}</li>
-          <li>Humidity: {humidity}%</li>
-          <li>Wind: {Math.round(wind)} km/h</li>
-          <li>
-            <img src={icon} />{" "}
+            <img src={weather.icon} alt={weather.description} />
           </li>
         </ul>
-      )}
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
