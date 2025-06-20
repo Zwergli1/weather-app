13c94cc49b0939b5a813445d29d04869f15f74d9
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2025-06-20 11:00:25 +0200

    Cleaned up unnecessary files

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import SearchEngine from "./SearchEngine";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <SearchEngine />
-        <footer>
-          Hosted on <a href="https://github.com/Zwergli1/weather-app">Github</a>
-        </footer>
-      </header>
+      <h1>Weather App</h1>
+      <SearchEngine />
+      <footer>
+        Hosted on <a href="https://github.com/Zwergli1/weather-app">Github</a>
+      </footer>
     </div>
   );
 }
